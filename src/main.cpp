5b8bbb5e6c5d39309d0844103fb238a68edd8fd8
test <test@test.com>
2016-05-25 10:07:23 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SubtreeTest
+//
+//  Created by Vincent Cheung on 25/5/2016.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
